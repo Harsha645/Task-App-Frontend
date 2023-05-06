c2b293a36de262c56c5c7c208345af3e7300ffd7
Harsha645 <harshapramod19980124@gmail.com>
2023-05-06 18:27:52 +0530

    Add moment dependency

--- src/component/TaskCard.jsx
+++ src/component/TaskCard.jsx
@@ -4,13 +4,30 @@ import { IoClose } from 'react-icons/io5'
 import { RiCloseCircleLine } from 'react-icons/ri'
 import StatesIcon from './StatesIcon'
 import CommonButton from './CommonButton'
+import moment from 'moment'
+import axios from 'axios'
 
 
 const TaskCard = (props) => {
-  const {title,description, dueDate, states} = props
+  const { id, title, description, dueDate, states,loadTasks } = props
+
+  const deleteTask = () => {
+    if (confirm("Are You Sure ?")) {
+      axios.delete("http://localhost:5000/" + id)
+        .then(res => {
+          if(res.status===200) {
+            alert("Task Deleted!")
+            loadTasks()
+          }
+        })
+        .catch(err => console.log(err))
+    }
+
+  }
+
   return (
     <div className='relative mt-5 p-5 border-2 border-neutral-900 shadow-2xl hover:scale-105 transition delay-75'>
-      <div className='absolute top-2 right-2 cursor-pointer'><RiCloseCircleLine className='text-4xl text-red-600' /></div>
+      <div className='absolute top-2 right-2 cursor-pointer'><RiCloseCircleLine className='text-4xl text-red-600' onClick={() => deleteTask(id)} /></div>
       <div className='flex justify-between  items-center text-4xl pt-5'>
         <div className=' space-y-4 '>
           <div >
@@ -18,13 +35,13 @@ const TaskCard = (props) => {
             <span className='font-bold'>{title}</span>
 
           </div>
-            <div>
-              <span className='font-semibold'>Description : </span>
-              <span className='font-bold'>{description}</span>
-            </div>
+          <div>
+            <span className='font-semibold'>Description : </span>
+            <span className='font-bold'>{description}</span>
+          </div>
           <div>
             <span className='font-semibold'>Date : </span>
-            <span className='font-bold'>{dueDate}</span>
+            <span className='font-bold'>{moment(dueDate).format("DD/MM/YYYY")}</span>
           </div>
 
         </div>
@@ -32,7 +49,7 @@ const TaskCard = (props) => {
 
 
           {states === "done" ? <StatesIcon icon={<TiTickOutline className='text-5xl text-white' />} bgColor={"bg-green-600"} />
-          :<StatesIcon icon={<IoClose className='text-5xl text-white'/>} bgColor ={"bg-red-600"}/>}
+            : <StatesIcon icon={<IoClose className='text-5xl text-white' />} bgColor={"bg-red-600"} />}
 
         </div>
       </div>
